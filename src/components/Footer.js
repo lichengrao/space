eb3f13e00764250518299b4b338e176f42acf276
Chan Rao <lichengrao@gmail.com>
2021-05-18 14:43:10 -0400

    Change name in title, header, footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      ©2020 StarLink Tracker. All Rights Reserved. Website Made by Chan Rao
+      ©2020 Space. All Rights Reserved. Website Made by Chan Rao
     </footer>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="App-header">
       <img src={starlink_logo} className="App-logo" alt="logo" />
-      <p className="title">Starlink Tracker</p>
+      <p className="title">Space: A Starlink Tracker</p>
     </header>
   );
 };
