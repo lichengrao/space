8fc92bbaace4af187394fc4e2b1b96ae3e797fc3
Chan Rao <lichengrao@gmail.com>
2021-05-18 12:53:41 -0400

    Convert SatelliteList to functional component

--- src/components/SatelliteList.js
+++ src/components/SatelliteList.js
@@ -1,74 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Spin, List, Avatar, Checkbox } from "antd";
 
 import satellite from "../assets/images/satellite.svg";
 
-class SatelliteList extends Component {
-  state = {
-    selected: [],
+const SatelliteList = (props) => {
+  const [selected, setSelected] = useState([]);
+
+  const showMap = () => {
+    props.onShowMap(selected);
   };
 
-  render() {
-    const { selected } = this.state;
-    const { satInfo, isLoad } = this.props;
-    const satList = satInfo ? satInfo.above : [];
-
-    return (
-      <div className="sat-list-box">
-        <Button
-          className="sat-list-btn"
-          size="large"
-          disabled={selected.length === 0}
-          onClick={this.showMap}
-        >
-          Track on the map
-        </Button>
-        <hr />
-        {isLoad ? (
-          <div className="spin-box">
-            <Spin tip="Loading..." size="large" />
-          </div>
-        ) : (
-          <List
-            className="sat-list"
-            itemLayout="horizontal"
-            dataSource={satList}
-            renderItem={(item) => (
-              <List.Item
-                actions={[
-                  <Checkbox dataInfo={item} onChange={this.onChange} />,
-                ]}
-              >
-                <List.Item.Meta
-                  avatar={
-                    <Avatar src={satellite} size="large" alt="satellite" />
-                  }
-                  title={<p>{item.satname}</p>}
-                  description={`Launch Date: ${item.launchDate}`}
-                />
-              </List.Item>
-            )}
-          />
-        )}
-      </div>
-    );
-  }
-
-  showMap = () => {
-    const { selected } = this.state;
-    this.props.onShowMap(selected);
-  };
-
-  onChange = (e) => {
+  const onChange = (e) => {
     const { dataInfo, checked } = e.target;
-    const { selected } = this.state;
-    const list = this.addOrRemove(dataInfo, checked, selected);
-    this.setState({
-      selected: list,
-    });
+    const list = addOrRemove(dataInfo, checked, selected);
+    setSelected(list);
   };
 
-  addOrRemove = (item, status, list) => {
+  const addOrRemove = (item, status, list) => {
     const found = list.some((entry) => entry.satid === item.satid);
 
     if (status && !found) {
@@ -82,11 +30,44 @@ class SatelliteList extends Component {
     return list;
   };
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevProps.satInfo !== this.props.satInfo) {
-      this.setState({ selected: [] });
-    }
-  }
-}
+  const { satInfo, isLoad } = props;
+  const satList = satInfo ? satInfo.above : [];
+
+  return (
+    <div className="sat-list-box">
+      <Button
+        className="sat-list-btn"
+        size="large"
+        disabled={selected.length === 0}
+        onClick={showMap}
+      >
+        Track on the map
+      </Button>
+      <hr />
+      {isLoad ? (
+        <div className="spin-box">
+          <Spin tip="Loading..." size="large" />
+        </div>
+      ) : (
+        <List
+          className="sat-list"
+          itemLayout="horizontal"
+          dataSource={satList}
+          renderItem={(item) => (
+            <List.Item
+              actions={[<Checkbox dataInfo={item} onChange={onChange} />]}
+            >
+              <List.Item.Meta
+                avatar={<Avatar src={satellite} size="large" alt="satellite" />}
+                title={<p>{item.satname}</p>}
+                description={`Launch Date: ${item.launchDate}`}
+              />
+            </List.Item>
+          )}
+        />
+      )}
+    </div>
+  );
+};
 
 export default SatelliteList;
