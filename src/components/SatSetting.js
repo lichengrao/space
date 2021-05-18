c6dac49ef2e9544b2d2722ee45ca5c1ceb4451ce
Chan Rao <lichengrao@gmail.com>
2021-05-18 13:55:14 -0400

    refactor: Migrate form from antd3 to antd4

--- src/components/SatSetting.js
+++ src/components/SatSetting.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Button, Form, InputNumber } from "antd";
 
-const SatSettingForm = ({ form, onShow }) => {
-  const { getFieldDecorator } = form;
-
+const SatSetting = ({ onShow }) => {
   const formItemLayout = {
     labelCol: {
       xs: { span: 24 },
@@ -15,106 +13,100 @@ const SatSettingForm = ({ form, onShow }) => {
     },
   };
 
-  const showSatellite = (e) => {
-    e.preventDefault();
-    form.validateFields((err, values) => {
-      if (!err) {
-        // console.log('Received values of form: ', values);
-        onShow(values);
-      }
-    });
+  const showSatellite = (values) => {
+    onShow(values);
   };
 
   return (
-    <Form {...formItemLayout} className="sat-setting" onSubmit={showSatellite}>
-      <Form.Item label="Longitude (degrees)">
-        {getFieldDecorator("longitude", {
-          rules: [
-            {
-              required: true,
-              message: "Please input your Longitude",
-            },
-          ],
-        })(
-          <InputNumber
-            min={-180}
-            max={180}
-            style={{ width: "100%" }}
-            placeholder="Please input Longitude"
-          />
-        )}
+    <Form {...formItemLayout} className="sat-setting" onFinish={showSatellite}>
+      <Form.Item
+        label="Longitude (degrees)"
+        name="longitude"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Longitude",
+          },
+        ]}
+      >
+        <InputNumber
+          min={-180}
+          max={180}
+          style={{ width: "100%" }}
+          placeholder="Please input Longitude"
+        />
       </Form.Item>
 
-      <Form.Item label="Latitude (degrees)">
-        {getFieldDecorator("latitude", {
-          rules: [
-            {
-              required: true,
-              message: "Please input your Latitude",
-            },
-          ],
-        })(
-          <InputNumber
-            placeholder="Please input Latitude"
-            min={-90}
-            max={90}
-            style={{ width: "100%" }}
-          />
-        )}
+      <Form.Item
+        label="Latitude (degrees)"
+        name="latitude"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Latitude",
+          },
+        ]}
+      >
+        <InputNumber
+          placeholder="Please input Latitude"
+          min={-90}
+          max={90}
+          style={{ width: "100%" }}
+        />
       </Form.Item>
 
-      <Form.Item label="Elevation (meters)">
-        {getFieldDecorator("elevation", {
-          rules: [
-            {
-              required: true,
-              message: "Please input your Elevation",
-            },
-          ],
-        })(
-          <InputNumber
-            placeholder="Please input Elevation"
-            min={-413}
-            max={8850}
-            style={{ width: "100%" }}
-          />
-        )}
+      <Form.Item
+        label="Elevation (meters)"
+        name="elevation"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Elevation",
+          },
+        ]}
+      >
+        <InputNumber
+          placeholder="Please input Elevation"
+          min={-413}
+          max={8850}
+          style={{ width: "100%" }}
+        />
       </Form.Item>
 
-      <Form.Item label="Altitude (degrees)">
-        {getFieldDecorator("altitude", {
-          rules: [
-            {
-              required: true,
-              message: "Please input your Altitude",
-            },
-          ],
-        })(
-          <InputNumber
-            placeholder="Please input Altitude"
-            min={0}
-            max={90}
-            style={{ width: "100%" }}
-          />
-        )}
+      <Form.Item
+        label="Altitude (degrees)"
+        name="altitude"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Altitude",
+          },
+        ]}
+      >
+        <InputNumber
+          placeholder="Please input Altitude"
+          min={0}
+          max={90}
+          style={{ width: "100%" }}
+        />
       </Form.Item>
 
-      <Form.Item label="Duration (secs)">
-        {getFieldDecorator("duration", {
-          rules: [
-            {
-              required: true,
-              message: "Please input your Duration",
-            },
-          ],
-        })(
-          <InputNumber
-            placeholder="Please input Duration"
-            min={0}
-            max={90}
-            style={{ width: "100%" }}
-          />
-        )}
+      <Form.Item
+        label="Duration (secs)"
+        name="duration"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Duration",
+          },
+        ]}
+      >
+        <InputNumber
+          placeholder="Please input Duration"
+          min={0}
+          max={90}
+          style={{ width: "100%" }}
+        />
       </Form.Item>
 
       <Form.Item className="show-nearby">
@@ -130,6 +122,4 @@ const SatSettingForm = ({ form, onShow }) => {
   );
 };
 
-const SatSetting1 = Form.create({ name: "satellite-setting" })(SatSettingForm);
-
-export default SatSetting1;
+export default SatSetting;
